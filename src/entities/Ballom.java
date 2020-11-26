eb3aeb0656b8e117efb60c91870ba3d51d616573
ngtrthinhTPC169 <69246359+ngtrthinhTPC169@users.noreply.github.com>
2020-11-26 16:37:52 +0700

    Up thiếu file nên up lại thôi :)

---
+++ src/entities/Ballom.java
@@ -0,0 +1,16 @@
+package entities;
+
+import graphics.Sprite;
+
+import java.util.ArrayList;
+
+public class Ballom extends GameCharacter {
+    public Ballom(int x, int y, Sprite sprite) {
+        super(x, y, sprite);
+    }
+
+    @Override
+    public void directionUpdate(ArrayList<String> input) {
+
+    }
+}
---
+++ src/entities/Oneal.java
@@ -0,0 +1,16 @@
+package entities;
+
+import graphics.Sprite;
+
+import java.util.ArrayList;
+
+public class Oneal extends GameCharacter {
+    public Oneal(int x, int y, Sprite sprite) {
+        super(x, y, sprite);
+    }
+
+    @Override
+    public void directionUpdate(ArrayList<String> input) {
+
+    }
+}
